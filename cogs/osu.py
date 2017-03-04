9f0fa7960f5a053dd9862fab004dad7cacc8278b
Cameron Fleming <cameron@ccwebs.co.uk>
2017-03-04 10:10:37 +0000

    Updates osu! APi collector, make it work!
    kek

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,18 +1,20 @@
 import requests
 import json
 
-print("osu! API v1.0\nBy runnerbeany\ngithub.com/runnerbeany\n")
+print("osu! API v1.1\nBy runnerbeany & Nevexo\ngithub.com/runnerbeany github.com/nevexo\n")
 
 class osu:
     def osuapi(query):
-        r = requests.get('https://osu.ppy.sh/api/get_user?u={0}&k=dfc290ab0fca5d8e54b6eb28d9134407b4723b48&u=username'.format(query))
-        dat = json.loads(r.text)
-        text = list[r.text]
-        userid = dat['user_id']
-        username = dat['username']
-        count50 = dat['count50']
+        r = requests.get('https://osu.ppy.sh/api/get_user?u={0}&k=dfc290ab0fca5d8e54b6eb28d9134407b4723b48'.format(query))
+        dat = r.json()
+        dat = dat[0]
+        usr = dat['username']
+        usrid = dat['user_id']
+        count300 = dat['count300']
+        playcount = dat['playcount']
+        acc = dat['accuracy']
         country = dat['country']
-        return userid,username,count50,country
+        return usr,usrid,count300,playcount,acc,country
 
         print(r.status_code)
         if r.status_code == 200:
